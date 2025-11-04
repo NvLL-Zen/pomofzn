43c79bfb1047d50e2521eab5d2a5f21b1492354e
NvLL-Zen <nvll.zen@gmail.com>
2025-11-04 20:32:11 +0700

    Renamed to pomo-noir

---
+++ pomo-noir/src/lib/bg_screensaver.svelte
@@ -0,0 +1,20 @@
+<script>
+    import media from '../medias/BW-SSaver.mp4'
+</script>
+
+<video autoplay muted loop id="backgroundScreensaver">
+    <source src={media} type="video/mp4" />
+</video>
+
+<style>
+    #backgroundScreensaver {
+        position: fixed;
+        right: 0;
+        bottom: 0;
+        min-width: 100%;
+        min-height: 100%;
+        z-index: 0;
+        object-fit: cover;
+        border: none;
+    }
+</style>
